5b9ac5dc8974efa19e72df2f588080c5e235fcfe
Bartosz Barc <bartosz.barc721@gmail.com>
2023-01-11 22:00:16 +0100

    Chenged JS and added buttons

--- js/script.js
+++ js/script.js
@@ -9,39 +9,42 @@
             done: true,
         },
     ]
-    
+
     const render = () => {
         let htmlString = "";
 
         for (const task of tasks) {
             htmlString += `
-            <li class="list__item ${task.done ? "list__item--done": ""}">
-                ${task.content}
+            <li class="list__item ${task.done ? "list__item--done" : ""}">
+                <button class="js-done">zrobione</button>${task.content}<button class="js-remove">usuń</button>
             </li>
             `;
         }
         document.querySelector(".js-tasks").innerHTML = htmlString;
     }
 
-    const addedNewTask = () =>{
-        const newTaskElement = document.querySelector(".js-newTask").value.trim();
+    const addedNewTask = (newTaskElement) => {
         tasks.push({
             content: newTaskElement,
             done: false,
         });
-        console.log(tasks)
+    }
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        const newTaskElement = document.querySelector(".js-newTask").value.trim();
+        if (newTaskElement === "") {
+            return
+        }
+        addedNewTask(newTaskElement);
+        document.querySelector(".js-newTask").value = "";
+        render();
     }
 
     const init = () => {
         render();
-
         const formElement = document.querySelector(".js-form");
-        formElement.addEventListener("submit", (event) =>{
-            event.preventDefault();
-            addedNewTask();
-            render();
-
-        })
+        formElement.addEventListener("submit", onFormSubmit)
 
     }
     init();
